296d67ccab4da1a0eb6cf0fab885fd9f305ec8a3
mirmik <mirmikns@yandex.ru>
2022-09-20 00:22:27 +0300

    'change dictionary merge method'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ directory = os.path.dirname(os.path.realpath(__file__))
 setup(
     name="bonesinger",
     packages=["bonesinger"],
-    version="0.2.0",
+    version="0.2.1",
     license="MIT",
     description="",
     author="mirmik",
