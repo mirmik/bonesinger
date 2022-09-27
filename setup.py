e36f87307c7d48458f4d84a59fed57e58700df05
mirmik <mirmikns@yandex.ru>
2022-09-27 11:44:34 +0300

    'add git build'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ directory = os.path.dirname(os.path.realpath(__file__))
 setup(
     name="bonesinger",
     packages=["bonesinger"],
-    version="0.2.1",
+    version="0.2.2",
     license="MIT",
     description="",
     author="mirmik",
