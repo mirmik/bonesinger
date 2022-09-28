a27840f4601b3d5ca0e4f3202be47b7c8fd57746
mirmik <mirmikns@yandex.ru>
2022-09-28 14:16:01 +0300

    'inc version'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ directory = os.path.dirname(os.path.realpath(__file__))
 setup(
     name="bonesinger",
     packages=["bonesinger"],
-    version="0.2.5",
+    version="0.2.6",
     license="MIT",
     description="",
     author="mirmik",
